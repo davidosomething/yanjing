edb8e7991bc9f1c85292d61785cfbd6a74c7096c
David O'Trakoun <me@davidosomething.com>
2021-03-17 02:13:18 -0400

    flush left, arch

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,19 +1,37 @@
+function flushLeft() {
+  var client = workspace.activeClient;
+  if (!client.moveable) {
+    return;
+  }
+
+  var rect = client.geometry;
+  rect.x = 0;
+  client.geometry = rect;
+}
+
+
 function flushRight() {
   var client = workspace.activeClient;
   if (!client.moveable) {
     return;
   }
 
-  var rect = client.frameGeometry;
+  var rect = client.geometry;
   var clientWidth = rect.width;
   var nextX = workspace.workspaceWidth - clientWidth;
 
-  client.geometry = Object.assign(rect, {
-    x: nextX
-  });
+  rect.x = nextX;
+  client.geometry = rect;
 }
 
 function main() {
+  registerShortcut(
+    'flush_left',
+    'Yanjing: Flush window to left edge of screen',
+    'ctrl+shift+meta+a',
+    flushLeft
+  );
+
   registerShortcut(
     'flush_right',
     'Yanjing: Flush window to right edge of screen',
