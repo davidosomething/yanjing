fa73a1a2b3149789d93d636ee876aff67f3a4fa7
David O'Trakoun <me@davidosomething.com>
2021-03-17 02:51:53 -0400

    it's pretty much working now

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
 KWin script to resize and move windows
 
+## Commands 
+
+`ctrl-shift-meta-a` - Flush the window to the LEFT side of the screen, or
+resize it if already flushed.
+
+`ctrl-shift-meta-d` - Flush the window to the RIGHT side of the screen, or
+resize it if already flushed.
+
+### Resizing logic
+
+Steps through [Sizes array](./contents/code/main.js), so from 25% of screen,
+to 33%, 50%, 66%, and then back around to 25%.
+
+## License
+
 MIT
--- contents/code/main.js
+++ contents/code/main.js
@@ -1,42 +1,115 @@
-function flushLeft() {
-  var client = workspace.activeClient;
-  if (!client.moveable) {
-    return;
+var Sizes = [
+  25,
+  33,
+  50,
+  66,
+];
+
+function cycle(client, dir) {
+  var rect = client.geometry;
+  var clientWidth = rect.width;
+
+  // E.g. if window is 650px and screen is 1080 we'd get 33
+  var intWidthPercent = Math.round(clientWidth / workspace.workspaceWidth * 100);
+
+  var sizeDiffs = Sizes.map(function (size) {
+    return ;
+  });
+
+  var smallestDiff = 9999;
+  var smallestI = 0;
+  Sizes.forEach(function (size, i) {
+    var diff = Math.abs(intWidthPercent - size);
+    if (diff < smallestDiff) {
+      smallestDiff = diff;
+      smallestI = i;
+    }
+  });
+  var nearestSize = Sizes[smallestI];
+  var nextI = smallestI === Sizes.length - 1 ? 0 : smallestI + 1;
+  var nextSize = Sizes[nextI];
+
+  var nextWidth = Math.floor(nextSize / 100 * workspace.workspaceWidth);
+  rect.width = nextWidth;
+  client.geometry = rect;
+
+  if (dir === 1) {
+    flushRight(client);
+  }
+}
+
+var FlushStates = {
+  NOOP: 0,
+  DONE: 1,
+  ERROR: 2,
+};
+
+function flushLeft(client) {
+  var rect = client.geometry;
+  var isFlushed = rect.x === 0;
+  if (isFlushed) {
+    return FlushStates.NOOP;
   }
 
-  var rect = client.geometry;
-  rect.x = 0;
-  client.geometry = rect;
+  if (client.moveable) {
+    var rect = client.geometry;
+    rect.x = 0;
+    client.geometry = rect;
+    return FlushStates.DONE;
+  }
+
+  print('Could not flush window left');
+  return FlushStates.ERROR;
+}
+
+function left() {
+  var client = workspace.activeClient;
+  if (flushLeft(client) === FlushStates.NOOP) {
+    cycle(client, -1);
+  }
 }
 
 
-function flushRight() {
-  var client = workspace.activeClient;
-  if (!client.moveable) {
-    return;
-  }
-
+function flushRight(client) {
   var rect = client.geometry;
   var clientWidth = rect.width;
-  var nextX = workspace.workspaceWidth - clientWidth;
+  var flushedX = workspace.workspaceWidth - clientWidth;
+  var isFlushed = rect.x === flushedX;
+  if (isFlushed) {
+    print(rect.x, flushedX);
+    return FlushStates.NOOP;
+  }
 
-  rect.x = nextX;
-  client.geometry = rect;
+  if (client.moveable) {
+    rect.x = flushedX;
+    client.geometry = rect;
+    return FlushStates.DONE;
+  }
+
+  print('Could not flush window right');
+  return FlushStates.ERROR;
+}
+
+function right() {
+  var client = workspace.activeClient;
+  if (flushRight(client) === FlushStates.NOOP) {
+    cycle(client, 1);
+  }
 }
 
 function main() {
   registerShortcut(
-    'flush_left',
-    'Yanjing: Flush window to left edge of screen',
+    'left',
+    'Yanjing: Flush or cyclic resize window to left edge of screen',
     'ctrl+shift+meta+a',
-    flushLeft
+    left
   );
 
   registerShortcut(
-    'flush_right',
-    'Yanjing: Flush window to right edge of screen',
+    'right',
+    'Yanjing: Flush or cyclic resize window to right edge of screen',
     'ctrl+shift+meta+d',
-    flushRight
+    right
   );
 }
 main();
